fb9ffc09ce3d62437a1bd2e23c2872811e32d733
yunscage <142385099+yunscage@users.noreply.github.com>
2024-09-20 10:30:08 +0800

    Create Demo.m

---
+++ Demo.m
@@ -0,0 +1,44 @@
+load('DataDemo.mat')
+Mylayers=[
+    featureInputLayer(9)
+    lstmLayer(27,"OutputMode","sequence");
+    tanhLayer
+    fullyConnectedLayer(81)
+    tanhLayer
+    fullyConnectedLayer(27)
+    tanhLayer
+    fullyConnectedLayer(9)
+    tanhLayer
+    fullyConnectedLayer(1)
+    % regressionLayer
+    ];
+
+MyOptions = trainingOptions('adam', ...
+    'MaxEpochs', 600, ...
+    'Shuffle', 'every-epoch', ...
+    'InitialLearnRate', 1e-3, ...
+    'Verbose', false, ...
+    'Plots', 'training-progress', ...
+    'L2Regularization', 0.0005, ... % 加入L2正则化
+    'ExecutionEnvironment', 'gpu'); % 使用GPU加速
+
+
+%% 使用自定义函数更新神经网络
+Prenet=dlnetwork(Mylayers);
+XTrain = dlarray(ThisDataX, 'CB'); 
+YTrain = dlarray(ThisDataY, 'CB'); 
+Thisnet=trainCustomNetwork(Prenet,XTrain,YTrain,MyOptions);
+% 允许继续训练 Allow continue train
+% Thisnet=trainCustomNetwork(Thisnet,XTrain,YTrain,MyOptions);
+ypred=forward(Thisnet,XTrain);
+
+
+% ypred=predict(Thisnet,ThisDataX);
+plot(ThisDataX(7,:),ThisDataY,'k');hold on;
+plot(ThisDataX(7,:),ypred,'r');
+rmsetotal=sqrt(mean((ThisDataY-ypred).^2));
+rmse1=sqrt(mean((ThisDataY(1:446)-ypred(1:446)).^2));
+disp('RMSEtotal=');
+disp(rmsetotal);
+disp('RMSE1=');
+disp(rmse1);
